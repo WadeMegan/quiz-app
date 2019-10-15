16ea8ed0fb6bf317d876190c2a46991f0d80f18f
wademegan96@gmail.com <meganwade@Megans-MacBook-Pro.local>
2019-10-15 17:30:57 -0500

    initial commit with wireframe, basic styling and images

---
+++ store.js
@@ -0,0 +1,123 @@
+const STORE=[
+//question 1
+{
+    question: 'Approximately how many pounds of pumpkins are produced in the US each year?',
+    options: [
+        '1.5 million pounds',
+        '3 million pounds',
+        '1.5 billion pounds',
+        '3 billion pounds'
+    ],
+    answer: '1.5 billion pounds',
+    additional: 'The US grows 1.5 billion pounds of pumpkins per year. Roughly 1.3 billions pounds of that is grown in preparation for Halloween!',
+},
+//question 2
+{
+    question: '',
+    options: [
+        '',
+        '',
+        '',
+        ''
+    ],
+    answer: '',
+    additional: '',
+},
+//question 3
+{
+    question: '',
+    options: [
+        '',
+        '',
+        '',
+        ''
+    ],
+    answer: '',
+    additional: '',
+},
+//question 4
+{
+    question: '',
+    options: [
+        '',
+        '',
+        '',
+        ''
+    ],
+    answer: '',
+    additional: '',
+},
+//question 5
+{
+    question: '',
+    options: [
+        '',
+        '',
+        '',
+        ''
+    ],
+    answer: '',
+    additional: '',
+},
+//question 6
+{
+    question: '',
+    options: [
+        '',
+        '',
+        '',
+        ''
+    ],
+    answer: '',
+    additional: '',
+},
+//question 7
+{
+    question: '',
+    options: [
+        '',
+        '',
+        '',
+        ''
+    ],
+    answer: '',
+    additional: '',
+},
+//question 8
+{
+    question: '',
+    options: [
+        '',
+        '',
+        '',
+        ''
+    ],
+    answer: '',
+    additional: '',
+},
+//question 9
+{
+    question: '',
+    options: [
+        '',
+        '',
+        '',
+        ''
+    ],
+    answer: '',
+    additional: '',
+},
+//question 10
+{
+    question: '',
+    options: [
+        '',
+        '',
+        '',
+        ''
+    ],
+    answer: '',
+    additional: '',
+}
+
+]
